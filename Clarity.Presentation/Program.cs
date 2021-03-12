0e29316c19583ab665fbd4077f357e40b26790f7
AnthonyJspr98 <60382856+AnthonyJspr98@users.noreply.github.com>
2021-03-12 17:34:14 +0800

    asdasd

--- Clarity.Presentation/Program.cs
+++ Clarity.Presentation/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Clarity.UI
+namespace Clarity.Presentation
 {
     public class Program
     {
